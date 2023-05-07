b1ba8a315387b5560faebfeb6bc94a371e0ad843
SuperCreativeUniqueUsername <gfrankiw@ualberta.ca>
2023-05-06 23:32:07 -0600

    pushing mu updates

---
+++ weather.py
@@ -0,0 +1,32 @@
+import asyncio
+import datetime
+
+from env_canada import ECWeather
+
+ec_en = ECWeather(coordinates=(53.619583, -113.943028))
+
+asyncio.run(ec_en.update())
+
+days = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    7: 'Sunday'
+}
+
+tempdic = { 
+    datetime.datetime.now().hour:ec_en.conditions["temperature"]["value"]
+    }
+
+print(tempdic[datetime.datetime.now().hour])
+
+# daily forecasts
+print(ec_en.daily_forecasts[0]['text_summary'])
+
+# hourly forecasts
+'''
+for i in ec_en.hourly_forecasts:
+    print(i['temperature'])'''
